ec5a1dd3042ddce8cdf27be02bff07ad9b5731c9
Hamidreza-Alimohammadi <alimohammadi.hamidreza@gmail.com>
2023-10-25 17:13:04 +0200

    first fresh commit of the finalized structure

---
+++ src/pynwb/tests/test_ecg.py
@@ -0,0 +1,269 @@
+
+from pynwb.testing import TestCase, remove_test_file, NWBH5IOMixin
+
+from pynwb import NWBHDF5IO, NWBFile
+from datetime import datetime
+from uuid import uuid4
+import numpy as np
+from dateutil.tz import tzlocal
+from hdmf.common import DynamicTable
+
+from ndx_ecg import (
+    CardiacSeries,
+    ECG,
+    HeartRate,
+    AuxiliaryAnalysis,
+    ECGChannelsGroup,
+    ECGRecDevice
+)
+
+
+def set_up_nwbfile(nwbfile: NWBFile = None):
+
+    nwbfile = NWBFile(
+        session_description='ECG test-rec session',
+        identifier=str(uuid4()),
+        session_start_time=datetime.now(tzlocal()),
+        experimenter='experimenter',
+        lab='DCL',
+        institution='UKW',
+        experiment_description='',
+        session_id='',
+    )
+    # define an endpoint main recording device
+    main_device = nwbfile.create_device(
+        name='name_of_the_MRD',  # MRD: main recording device
+        description='description_of_the_MRD',
+        manufacturer='manufacturer_of_the_MRD'
+    )
+
+    '''
+    creating an ECG electrodes table
+    as a DynamicTable
+    '''
+    ecg_electrodes_table = DynamicTable(
+        name='ecg_electrodes',
+        description='info on ECG electrodes'
+    )
+
+    # add relevant columns
+    ecg_electrodes_table.add_column(
+        name='electrode_name',
+        description='reference name of the corresponding electrode'
+    )
+    ecg_electrodes_table.add_column(
+        name='electrode_location',
+        description='the location of the corresponding electrode on the body'
+    )
+    ecg_electrodes_table.add_column(
+        name='electrode_info',
+        description='descriptive information on the corresponding electrode'
+    )
+
+    # add electrodes
+    ecg_electrodes_table.add_row(
+        electrode_name='el_0',
+        electrode_location='right upper-chest',
+        electrode_info='descriptive info'
+    )
+    ecg_electrodes_table.add_row(
+        electrode_name='el_1',
+        electrode_location='left lower-chest',
+        electrode_info='descriptive info'
+    )
+    ecg_electrodes_table.add_row(
+        electrode_name='reference',
+        electrode_location='top of the head',
+        electrode_info='descriptive info'
+    )
+    # adding the object of DynamicTable
+    nwbfile.add_analysis(ecg_electrodes_table)  # storage point for custom DT and NWB-C
+
+    '''
+    creating an ECG recording channels table
+    as a DynamicTable
+    '''
+    recording_channels_table = DynamicTable(
+        name='recording_channels',
+        description='info on ecg recording channels'
+    )
+
+    # add relevant columns
+    recording_channels_table.add_column(
+        name='channel_name',
+        description='reference name of the corresponding recording channel'
+    )
+    recording_channels_table.add_column(
+        name='channel_type',
+        description='type of the recording, e.g., single electrode or differential'
+    )
+    recording_channels_table.add_column(
+        name='electrodes',
+        description='descriptive information the corresponding electrode(s)',
+    )
+
+    # add channels
+    recording_channels_table.add_row(
+        channel_name='ch_0',
+        channel_type='single',
+        electrodes='el_0'
+    )
+    recording_channels_table.add_row(
+        channel_name='ch_1',
+        channel_type='differential',
+        electrodes='el_0 and el_1'
+    )
+    # adding the object of DynamicTable
+    nwbfile.add_analysis(recording_channels_table)  # storage point for custom DT and NWB-C
+
+    return nwbfile, ecg_electrodes_table, recording_channels_table
+
+
+class TestCardiacSeriesRoundtrip(TestCase):
+
+    def setUp(self):
+        self.nwbfile = set_up_nwbfile()[0]
+        self.electrodes_table = set_up_nwbfile()[1]
+        self.channels_table = set_up_nwbfile()[2]
+        self.path = "test.nwb"
+
+    def tearDown(self):
+        remove_test_file(self.path)
+
+    def test_roundtrip(self):
+        # define an ECGRecDevice-type device for ecg recording
+        ecg_device = ECGRecDevice(
+            name='name_of_the_ECGRD',
+            description='description_of_the_ECGRD',
+            manufacturer='manufacturer_of_the_ECGRD',
+            filtering='notch-60Hz-analog',
+            gain='100',
+            offset='0',
+            synchronization='taken care of via ...',
+            endpoint_recording_device=self.nwbfile.get_device('name_of_the_MRD')
+        )
+        # adding the object of ECGRecDevice
+        self.nwbfile.add_device(ecg_device)
+
+        ecg_channels_group = ECGChannelsGroup(
+            name='ecg_channels_group',
+            group_description='a group to store electrodes and channels table, and linking to ECGRecDevice.',
+            electrodes=self.nwbfile.get_analysis('ecg_electrodes'),
+            channels=self.nwbfile.get_analysis('recording_channels'),
+            recording_device=ecg_device
+        )
+        # adding the object of ECGChannelsGroup
+        self.nwbfile.add_analysis(ecg_channels_group)  # storage point for custom DT and NWB-C
+        #
+        #
+        # storing the ECG data
+        dum_data_ecg = np.random.randn(20, 2)
+        dum_time_ecg = np.linspace(0, 10, len(dum_data_ecg))
+        ecg_cardiac_series = CardiacSeries(
+            name='ecg_raw_CS',
+            data=dum_data_ecg,
+            timestamps=dum_time_ecg,
+            unit='mV',
+            channels_group=ecg_channels_group
+        )
+
+        ecg_raw = ECG(
+            cardiac_series=ecg_cardiac_series,
+            processing_description='raw acquisition'
+        )
+        # adding the raw acquisition of ECG to the nwb_file inside an 'ECG' container
+        self.nwbfile.add_acquisition(ecg_raw)
+
+        # storing the HeartRate data
+        dum_data_hr = np.random.randn(10, 2)
+        dum_time_hr = np.linspace(0, 10, len(dum_data_hr))
+        hr_cardiac_series = CardiacSeries(
+            name='heart_rate_CS',
+            data=dum_data_hr,
+            timestamps=dum_time_hr,
+            unit='bpm',
+            channels_group=ecg_channels_group
+        )
+
+        # defining an ecg_module to store the processed cardiac data and analysis
+        ecg_module = self.nwbfile.create_processing_module(
+            name='cardio_module',
+            description='a module to store processed cardiac data'
+        )
+
+        hr = HeartRate(
+            cardiac_series=hr_cardiac_series,
+            processing_description='processed heartRate of the animal'
+        )
+        # adding the heart rate data to the nwb_file inside an 'HeartRate' container
+        ecg_module.add(hr)
+
+        # storing the Auxiliary data
+        # An example could be the concept of ceiling that is being used in the literature published by DCL@UKW
+        dum_data_ceil = np.random.randn(10, 2)
+        dum_time_ceil = np.linspace(0, 10, len(dum_data_ceil))
+        ceil_cardiac_series = CardiacSeries(
+            name='heart_rate_ceil_CS',
+            data=dum_data_ceil,
+            timestamps=dum_time_ceil,
+            unit='bpm',
+            channels_group=ecg_channels_group
+        )
+
+        ceil = AuxiliaryAnalysis(
+            cardiac_series=ceil_cardiac_series,
+            processing_description='processed auxiliary analysis'
+        )
+        # adding the 'ceiling' auxiliary analysis to the nwb_file inside an 'AuxiliaryAnalysis' container
+        ecg_module.add(ceil)
+
+        # storing the processed heart rate: as an NWBDataInterface with the new assigned name instead of default
+        # An example could be the concept of HR2ceiling that is being used in the literature published by DCL@UKW
+        dum_data_hr2ceil = np.random.randn(10, 2)
+        dum_time_hr2ceil = np.linspace(0, 10, len(dum_data_hr2ceil))
+        hr2ceil_cardiac_series = CardiacSeries(
+            name='heart_rate_to_ceil_CS',
+            data=dum_data_hr2ceil,
+            timestamps=dum_time_hr2ceil,
+            unit='bpm',
+            channels_group=ecg_channels_group
+        )
+
+        hr2ceil = HeartRate(
+            name='HR2Ceil',
+            cardiac_series=hr2ceil_cardiac_series,
+            processing_description='processed heartRate to ceiling'
+        )
+        # adding the 'HR2ceiling' processed HR to the nwb_file inside an 'HeartRate' container
+        ecg_module.add(hr2ceil)
+
+        # writing the nwbfile on disk
+        with NWBHDF5IO(self.path, mode='w') as io:
+            io.write(self.nwbfile)
+
+        # reading and asserting
+        with NWBHDF5IO(self.path, mode="r", load_namespaces=True) as io:
+            read_nwbfile = io.read()
+            # assertion of ECG raw acquisition
+            self.assertContainerEqual(ecg_raw, read_nwbfile.acquisition['ECG'])
+            self.assertEqual('raw acquisition', read_nwbfile.acquisition['ECG'].processing_description)
+
+            # CardiacSeries assertion
+            self.assertContainerEqual(ecg_cardiac_series, read_nwbfile.acquisition['ECG']['ecg_raw_CS'])
+
+            # assertion of the arguments of a typical CardiacSeries
+            np.testing.assert_array_equal(dum_data_ecg, read_nwbfile.acquisition['ECG']['ecg_raw_CS'].data[:])
+            np.testing.assert_array_equal(dum_time_ecg, read_nwbfile.acquisition['ECG']['ecg_raw_CS'].timestamps[:])
+            self.assertEqual('mV', read_nwbfile.acquisition['ECG']['ecg_raw_CS'].unit)
+            self.assertContainerEqual(ecg_channels_group, read_nwbfile.acquisition['ECG']['ecg_raw_CS'].channels_group)
+
+            # assertion of default(first three)/given(last) names of interfaces
+            self.assertEqual('ECG', read_nwbfile.acquisition['ECG'].name)
+            self.assertEqual('HeartRate', read_nwbfile.processing['cardio_module']['HeartRate'].name)
+            self.assertEqual('AuxiliaryAnalysis', read_nwbfile.processing['cardio_module']['AuxiliaryAnalysis'].name)
+            self.assertEqual('HR2Ceil', read_nwbfile.processing['cardio_module']['HR2Ceil'].name)
+
+            # assertion of ECGChannelsGroup elements (instances of DynamicTable)
+            self.assertEqual(self.electrodes_table, read_nwbfile.analysis['ecg_channels_group'].electrodes)
+            self.assertEqual(self.channels_table, read_nwbfile.analysis['ecg_channels_group'].channels)
+
---
+++ src/spec/create_extension_spec.py
@@ -0,0 +1,243 @@
+# -*- coding: utf-8 -*-
+import os.path
+
+from pynwb.spec import (NWBNamespaceBuilder, export_spec,
+                        NWBGroupSpec, NWBAttributeSpec, NWBDatasetSpec, NWBLinkSpec)
+
+
+def main():
+    # these arguments were auto-generated from your cookiecutter inputs
+    ns_builder = NWBNamespaceBuilder(
+        doc="""This extension is developed to extend NWB data standards to incorporate ECG recordings.""",
+        name="""ndx-ecg""",
+        version="""0.1.0""",
+        author=list(map(str.strip, """Hamidreza Alimohammadi ([AT]DefenseCircuitsLab)""".split(','))),
+        contact=list(map(str.strip, """alimohammadi.hamidreza@gmail.com""".split(',')))
+    )
+
+    ns_builder.include_type('TimeSeries', namespace='core')
+    ns_builder.include_type('DynamicTable', namespace='hdmf-common')
+    ns_builder.include_type('Device', namespace='core')
+    ns_builder.include_type('NWBDataInterface', namespace='core')
+    ns_builder.include_type('NWBContainer', namespace='core')
+
+    cardiac_series = NWBGroupSpec(
+        neurodata_type_def='CardiacSeries',
+        neurodata_type_inc='TimeSeries',
+        doc='A group to store standardized cardiac time-series, e.g., ECG and HeartRate, including '
+            'acquisitions, processed cardiac series or auxiliary analysis.',
+        datasets=[
+            NWBDatasetSpec(
+                name='data',
+                dtype='numeric',
+                doc='Associated cardiac-series data. Note that in general, this data could be a numerical array,'
+                    'the columns of which corresponding to the recording channels, indexed similarly as the rows '
+                    'of the dynamic table passed for the channels argument. Notice the difference between electrodes '
+                    'table and the recording channels.',
+                attributes=[
+                    NWBAttributeSpec(
+                        name='unit',
+                        dtype='text',
+                        doc='Default measurement unit of the corresponding cardiac-series.'
+                    ),
+                    NWBAttributeSpec(
+                        name='which_channels',
+                        dtype='text',
+                        value='One to one map with all the channels in ECGRecDevice.',
+                        doc='Determine that the columns of data here corresponds to which recording channels. '
+                            'By default it is meant to be a one to one map to the recording channels table, passed '
+                            'to the ECGRecDevice container. A use case can be like <0: ch_0, 1:ch_2>, implying '
+                            'that the data is of two columns and the 0-th and 1-th columns correspond to ch_0 and '
+                            'ch_2 of all the recording channels, respectively.'
+                    ),
+                    NWBAttributeSpec(
+                        name='conversion',
+                        dtype='float32',
+                        value=1.0,
+                        doc='Global conversion factor for corresponding cardiac-series. See also channel_conversion. '
+                            'finally, the stored data-set for each channel will be returned in default unit using '
+                            'the scaling as: data-in-default-unit = data * conversion * channel_conversion.'
+                    )
+                ]
+            ),
+            NWBDatasetSpec(
+                name='channel_conversion',
+                dtype='float32',
+                quantity='?',
+                doc='Conventional NWB channel-specific conversion factor. Default value would be 1. '
+                    'See also conversion. Finally, the stored data-set for each channel will be returned in '
+                    'default unit using the scaling as: '
+                    'data-in-default-unit = data * conversion * channel_conversion.',
+                attributes=[
+                    NWBAttributeSpec(
+                        name='axis',
+                        dtype='int32',
+                        value=1,
+                        doc='Zero-indexed axis of the data that the channel-specific conversion factor corresponds to.'
+                    )
+                ]
+            )
+        ],
+        links=[
+            NWBLinkSpec(
+                name='channels_group',
+                target_type='ECGChannelsGroup',
+                doc='Link to the ECGChannelsGroup as a reference for recording channels, recording electrodes '
+                    'and the recording device.'
+            )
+        ]
+    )
+
+    ecg_series = NWBGroupSpec(
+        neurodata_type_def='ECG',
+        neurodata_type_inc='NWBDataInterface',
+        default_name='ECG',
+        doc='Specific data interface for ElectroCardioGram time-series (raw/processed).',
+        attributes=[
+            NWBAttributeSpec(
+                name='processing_description',
+                doc='Explain how the time-series is processed or whether it is a raw acquisition.',
+                dtype='text',
+                required=False
+            )
+        ],
+        groups=[
+            NWBGroupSpec(
+                neurodata_type_inc='CardiacSeries',
+                doc='CardiacSeries object representing the ElectroCardioGram (ECG). Whether raw or processed.',
+                quantity='+'
+            )
+        ]
+    )
+
+    hr_series = NWBGroupSpec(
+        neurodata_type_def='HeartRate',
+        neurodata_type_inc='NWBDataInterface',
+        default_name='HeartRate',
+        doc='Specific data interface for HeartRate.',
+        attributes=[
+            NWBAttributeSpec(
+                name='processing_description',
+                doc='Explain how the time-series is processed or whether it is a raw acquisition.',
+                dtype='text',
+                required=False
+            )
+        ],
+        groups=[
+            NWBGroupSpec(
+                neurodata_type_inc='CardiacSeries',
+                doc='CardiacSeries object representing the HearRate (HR). Whether raw or processed.',
+                quantity='+'
+            )
+        ]
+    )
+
+    aux_analysis = NWBGroupSpec(
+        neurodata_type_def='AuxiliaryAnalysis',
+        neurodata_type_inc='NWBDataInterface',
+        default_name='AuxiliaryAnalysis',
+        doc='Specific data interface for whatever relevant auxiliary in-between analysis.',
+        attributes=[
+            NWBAttributeSpec(
+                name='processing_description',
+                doc='Explain how the analysis is processed.',
+                dtype='text',
+                required=False
+            )
+        ],
+        groups=[
+            NWBGroupSpec(
+                neurodata_type_inc='CardiacSeries',
+                doc='CardiacSeries object representing the auxiliary analysis.',
+                quantity='+'
+            )
+        ]
+    )
+
+    ecg_recording_channels = NWBGroupSpec(
+        neurodata_type_def='ECGChannelsGroup',
+        neurodata_type_inc='NWBContainer',
+        doc='Information of all channels from which the corresponding CardiacSeries is generated. Note that these '
+            'channels can represent single recording electrodes or differential recordings.',
+        attributes=[
+            NWBAttributeSpec(
+                name='group_description',
+                doc='Describe the recording channels for this specific experiment session.',
+                dtype='text',
+                required=False
+            )
+        ],
+        groups=[
+            NWBGroupSpec(
+                name='electrodes',
+                neurodata_type_inc='DynamicTable',
+                doc='A dynamic table of the implemented electrodes.',
+            ),
+            NWBGroupSpec(
+                name='channels',
+                neurodata_type_inc='DynamicTable',
+                doc='A dynamic table of the ECG recording channels.',
+            )
+        ],
+        links=[
+            NWBLinkSpec(
+                name='recording_device',
+                target_type='ECGRecDevice',
+                doc='Link to the ECGRecDevice used to record cardiac signals.'
+            )
+        ]
+    )
+
+    ecg_recording_device = NWBGroupSpec(
+        neurodata_type_def='ECGRecDevice',
+        neurodata_type_inc='Device',
+        doc='ECG recording device.',
+        attributes=[
+            NWBAttributeSpec(
+                name='filtering',
+                doc='Explain analogue frequency filtering of the ECG acquisition device, '
+                    'if any is implemented.',
+                dtype='text',
+                required=False
+            ),
+            NWBAttributeSpec(
+                name='gain',
+                doc='Explain the gain settings of the ECG acquisition device.',
+                dtype='text',
+                required=False
+            ),
+            NWBAttributeSpec(
+                name='offset',
+                doc='Explain what the baseline of the ECG signal is set to.',
+                dtype='text',
+                required=False
+            ),
+            NWBAttributeSpec(
+                name='synchronization',
+                doc='Explain the synchronization settings if the ECG recording device is separately connected to '
+                    'another recording system.',
+                dtype='text',
+                required=False
+            )
+        ],
+        links=[
+            NWBLinkSpec(
+                name='endpoint_recording_device',
+                target_type='Device',
+                doc='endpoint recording device to which the ECG recording device is connected.'
+            )
+        ]
+    )
+
+    new_data_types = [cardiac_series, ecg_series, hr_series, aux_analysis,
+                      ecg_recording_channels, ecg_recording_device]
+
+    # export the spec to yaml files in the spec folder
+    output_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'spec'))
+    export_spec(ns_builder, new_data_types, output_dir)
+    print('Spec files generated. Please make sure to rerun `pip install .` to load the changes.')
+
+
+if __name__ == '__main__':
+    # usage: python create_extension_spec.py
+    main()
